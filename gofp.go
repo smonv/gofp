2f6434e985438b3c1d578433455a215cbb99eace
Tien Thanh <thanhvt1609@gmail.com>
2015-08-05 23:12:22 +0700

    finish first version

---
+++ gofp.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	flag.Parse()
+	src_dir := flag.Arg(0)
+	if src_dir == "" {
+		src_dir = "/mnt"
+	}
+
+	log.Println("Checking: " + src_dir)
+
+	src, err := os.Stat(src_dir)
+	if err != nil {
+		panic(err)
+	}
+
+	if src.IsDir() {
+		CheckPermission(src_dir)
+	} else {
+		log.Println("Source path not is directory")
+	}
+	os.Exit(1)
+}
+
+func CheckPermission(dirPath string) {
+	fullPath, err := filepath.Abs(dirPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = filepath.Walk(fullPath, walkFunc)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func walkFunc(path string, file os.FileInfo, err error) error {
+	if file.IsDir() {
+		if file.Mode().String() != "drwxrwxr-x" {
+			err = os.Chmod(path, 0775)
+			displayLog(file, err)
+		}
+	} else {
+		if file.Mode().String() != "-rw-rw-r--" {
+			err = os.Chmod(path, 0664)
+			displayLog(file, err)
+		}
+	}
+	return nil
+}
+
+func displayLog(file os.FileInfo, err error) {
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Fixed: ", file.Name())
+	}
+}
---
+++ gofp_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"path"
+	"testing"
+)
+
+func TestCheckPermission(t *testing.T) {
+	test_dir := "/tmp/gofp"
+	os.Mkdir(test_dir, 0777)
+	// because umask=0002 so cannot create dir with 0777 permission
+	os.Chmod(test_dir, 0777)
+	defer os.RemoveAll(test_dir)
+
+	tmp := [][]string{{"dir1", "file1"}, {"dir2", "file2"}, {"dir3", "file3"}}
+	for _, v := range tmp {
+		dir_path := path.Join(test_dir, v[0])
+		os.Mkdir(dir_path, 0777)
+		os.Chmod(dir_path, 0777)
+		file, _ := os.Create(path.Join(dir_path, v[1]))
+		file.Chmod(0666)
+	}
+
+	CheckPermission(test_dir)
+
+	for _, v := range tmp {
+		dir_path := path.Join(test_dir, v[0])
+		dir, _ := os.Stat(dir_path)
+		if dir.Mode().String() != "drwxrwxr-x" {
+			t.Error(dir.Name(), "wrong permission: ", dir.Mode())
+		}
+
+		file, _ := os.Stat(path.Join(dir_path, v[1]))
+		if file.Mode().String() != "-rw-rw-r--" {
+			t.Error(file.Name(), "wrong permission: ", file.Mode())
+		}
+	}
+}
