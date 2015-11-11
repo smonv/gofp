6e85acf502a93574eccc0957625fe657a85bd909
Tien Thanh <thanhvt1609@gmail.com>
2015-11-11 11:03:50 +0700

    use flag.String instead of flag.Args

--- gofp.go
+++ gofp.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 type Result struct {
@@ -17,36 +16,40 @@ type Result struct {
 func main() {
 	defer os.Exit(1)
 
+	dirPath := flag.String("path", "", "directory path")
 	flag.Parse()
-	dirPath := strings.TrimSpace(flag.Arg(0))
-	result := &Result{0, 0}
-	result = CheckPermission(dirPath, result)
-	if result != nil {
-		fmt.Println("Total Fixed Dir: ", result.Dir)
-		fmt.Println("Total Fixed File: ", result.File)
+	if *dirPath != "" {
+		result := &Result{0, 0}
+		result = CheckPermission(*dirPath, result)
+		if result != nil {
+			fmt.Println("Total Fixed Dir: ", result.Dir)
+			fmt.Println("Total Fixed File: ", result.File)
+		}
+	} else {
+		log.Panic("Please input path to check!")
 	}
 }
 
 func CheckPermission(dirPath string, r *Result) (result *Result) {
-	if dirPath == "" {
-		log.Println("Please input path to check!")
-	} else {
-		src, err := os.Stat(dirPath)
-		if err != nil {
+	src, err := os.Stat(dirPath)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	if src.IsDir() {
+		log.Println("Checking: " + dirPath)
+		fullPath, _ := filepath.Abs(dirPath)
+
+		if err = filepath.Walk(fullPath, r.Visit); err != nil {
 			log.Println(err)
-			return nil
-		}
-		if src.IsDir() {
-			log.Println("Checking: " + dirPath)
-			fullPath, _ := filepath.Abs(dirPath)
-			if err = filepath.Walk(fullPath, r.Visit); err != nil {
-				log.Println(err)
-			} else {
-				return r
-			}
 		} else {
-			log.Println("Source path not is directory")
+			return r
 		}
+
+	} else {
+		log.Fatal("Source path not is directory")
 	}
 	return nil
 }
